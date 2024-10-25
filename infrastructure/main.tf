ddd47dc8ef6c77aa3c34fba20093b2af0b17e93f
ardhendusgit <crackednutxx@gmail.com>
2024-10-25 14:57:07 +0530

    separated security groups

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -31,72 +31,11 @@ resource "aws_instance" "http_instance" {
     EOF
 }
 
-resource "aws_security_group" "http_server_security_group" {
-  name = "http-server-sg"
-}
-
-resource "aws_security_group_rule" "allow_http_inbound" {
-  type              = "ingress"
-  security_group_id = aws_security_group.http_server_security_group.id
-
-  from_port                = 8000
-  to_port                  = 8000
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.alb.id
-}
-
-resource "aws_security_group_rule" "allow_all_outbound" {
-  type              = "egress"
-  security_group_id = aws_security_group.http_server_security_group.id
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-
-resource "aws_security_group" "alb" {
-  name = "alb-security-group"
-}
-
 resource "aws_lb" "load_balancer" {
   name               = "http-server-lb"
   load_balancer_type = "application"
   subnets            = data.aws_subnet_ids.default_subnet.ids
   security_groups    = [aws_security_group.alb.id]
-
-}
-
-resource "aws_security_group_rule" "allow_alb_https_inbound" {
-  type              = "ingress"
-  security_group_id = aws_security_group.alb.id
-
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-}
-
-resource "aws_security_group_rule" "allow_alb_http_inbound" {
-  type              = "ingress"
-  security_group_id = aws_security_group.alb.id
-
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-}
-
-resource "aws_security_group_rule" "allow_alb_http_outbound" {
-  type              = "egress"
-  security_group_id = aws_security_group.alb.id
-
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
 
 }
 
